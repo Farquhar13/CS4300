757b8d65713ede3bd5610c63906b0efd7a92bda0
Farquhar13 <farquhar13@gmail.com>
2019-04-22 18:03:01 -0400

    changing paths for heroku

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -15,7 +15,6 @@ import re
 project_name = "S"
 net_id = "...."
 
-
 @irsystem.route('/', methods=['GET'])
 def search():
 	#query = request.args.get('search')
@@ -23,55 +22,85 @@ def search():
 	MinPrice = request.args.get('MinPrice')
 	MaxPrice = request.args.get('MaxPrice')
 	color  = request.args.get('color')
-	Flavor	= request.args.get('Flavor1')
-	print(Flavor)
+	Flavor_Additional= request.args.get('Flavor_1')
+	#print(MinPrice)
+	#print(WineType)
+	if MinPrice is "":
+		MinPrice = 0
+	if MaxPrice is "":
+		MaxPrice = 3300
 	if not color:
 		data = []
 		output_message = ''
 	else:
-		## comment to update code
-		master_data = pd.read_pickle("/home/farquhar13/CS4300_Flask_template/app/data/master_data_lean.pkl")
-		vin_tfidf = np.load('/home/farquhar13/CS4300_Flask_template/app/data/vin_tfidf.npy').item()
-		word_to_index = np.load('/home/farquhar13/CS4300_Flask_template/app/data/word_to_index.npy').item()
+
+		print(MinPrice)
+		print(MaxPrice)
+
+	## comment to update code
+		master_data = pd.read_pickle("~/app/data/master_data_lean.pkl")
+		vin_tfidf = np.load('~/app/data/vin_tdidf.npy').item()
+		word_to_index = np.load('~/app/data/word_to_index.npy').item()	
 
 
 		def query_vec(s):
+
 			s = s.lower()
 			s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
 			tokens = [token for token in s.split(" ") if token != ""]
-			bigram = [x[0]+" "+ x[1] for x in list(ngrams(tokens, 2))]
-			query_token = tokens + bigram
+			query_token = tokens
 			query_vec = np.zeros((1,len(word_to_index)))
 			for i,v in enumerate(query_token):
 				if v in word_to_index:
 					query_vec[0,word_to_index[v]] = 1
+
 			return query_vec
 
 
 		def similar_vin(query_vec, vin_tfidf):
-			sims = np.dot(vin_tfidf.toarray(),query_vec.T)
-			asort = np.argsort(-sims,axis = 0)
-			return asort
+
+			subset = []
+			for i,v in enumerate(query_vec[0,:]):
+				if v != 0:
+					subset.append(i)
+
+			tfidf = vin_tfidf[:,subset]
+			query_vec = query_vec[0,subset]
+
+			sims = np.dot(tfidf.toarray(),query_vec.T)
+			sim_mat = pd.DataFrame(sims)
+			sim_mat.columns = ['sims']
+			sim_mat['index'] = np.arange(len(sim_mat))
+
+			return sim_mat
 
 
 		def shortlist_vin(query_vec, color = None, price_min = None, price_max = None):
-			master_data_cpy = master_data.copy(deep = True)
-			reccommendation = similar_vin(query_vec,vin_tfidf)
-			chk = pd.DataFrame(reccommendation)
-			chk.columns = ['index']
-			chk1 = pd.merge(chk, master_data_cpy, on='index', how='left')
+			sim_mat = similar_vin(query_vec,vin_tfidf)
+			recco_mat = pd.merge(sim_mat, master_data, on='index', how='left')
 
 			if color != None:
-				chk1 =  chk1[chk1.Color == color]
+				recco_mat =  recco_mat[recco_mat.Color == color]
 
 			if price_min != None and price_max != None:
-				chk1 = chk1[chk1.price < price_max]
-				chk1 = chk1[chk1.price > price_min]
-			return chk1
+				recco_mat = recco_mat[recco_mat.price < price_max]
+				recco_mat = recco_mat[recco_mat.price > price_min]
 
-		output_message = "Searched"
+			recco_mat.sort_values("sims", axis = 0, ascending = False,
+						 inplace = True)
+
+			return recco_mat[0:10]
+
+
+
+
+
+		output_message = "Searched for " + color + " Wine"
 
 		#data = "You want " + query + " Wine " + " in " + price
+		if WineType is not None and Flavor_Additional is not None:
+			WineType = WineType + " " +Flavor_Additional
+		#print(WineType)
 
 		query = query_vec(WineType)
 		shortlist = shortlist_vin(query, color = color, price_min = int(MinPrice), price_max = int(MaxPrice))
