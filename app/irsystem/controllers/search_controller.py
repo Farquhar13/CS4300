df2d141795add7c92cd7f575c3963d7b522307f3
Farquhar13 <farquhar13@gmail.com>
2019-04-28 17:29:45 -0400

    new data

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,14 +11,17 @@ from sklearn.preprocessing import normalize
 from nltk import word_tokenize
 from nltk.util import ngrams
 import re
+#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+#import matplotlib
+
 
 # relative path stuff
 import os
 dirname = os.path.dirname(__file__)
-data_path = os.path.join(dirname, '..', '..', 'data', 'master_data_lean.pkl')
-tfidf_path = os.path.join(dirname, '..', '..', 'data', 'vin_tfidf.npy')
+master_data_path = os.path.join(dirname, '..', '..', 'data', 'master_data_lean.pkl')
+vin_tfidf_path = os.path.join(dirname, '..', '..', 'data', 'vin_tfidf.npy')
 word_to_index_path = os.path.join(dirname, '..', '..', 'data', 'word_to_index.npy')
-
+variety_tfidf_path = os.path.join(dirname, '..', '..', 'data', 'variety_tfidf.npy')
 
 project_name = "S"
 net_id = "...."
@@ -33,22 +36,34 @@ def search():
 	Flavor_Additional= request.args.get('Flavor_1')
 	#print(MinPrice)
 	#print(WineType)
-	if MinPrice is "":
+	if not MinPrice:
 		MinPrice = 0
-	if MaxPrice is "":
+	if not MaxPrice:
 		MaxPrice = 3300
 	if not color:
-		data = []
-		output_message = ''
-	else:
+		color = None
+	#else:
+	print(MinPrice)
+	print(MaxPrice)
 
-		print(MinPrice)
-		print(MaxPrice)
+	price = 0
+	sim = 0
+	if not WineType and not Flavor_Additional:
+		data = []
+		output_message = "Please enter a flavour"
+	else:
+		if not WineType:
+			WineType = ""
+		if not Flavor_Additional:
+			Flavor_Additional = ""
+		WineType = WineType + " " +Flavor_Additional
+		print(WineType)
 
 	## comment to update code
-		master_data = pd.read_pickle(data_path)
-		vin_tfidf = np.load(tfidf_path).item()
+		master_data = pd.read_pickle(master_data_path)
+		vin_tfidf = np.load(vin_tfidf_path).item()
 		word_to_index = np.load(word_to_index_path).item()
+		#wine_mask = np.load('D:\\Cornell_Acads\\Second_Semester\\CS4300_Flask_template-master\\app\\irsystem\\controllers\\wine_mask.npy')
 
 
 		def query_vec(s):
@@ -101,17 +116,30 @@ def search():
 
 
 
+		if color  ==None:
+			output_message = "Showing Results for "+ WineType + " Wine"
+		else:
+			output_message = "Showing Results for " + color + " " + WineType + " Wine"
+
+	#data = "You want " + query + " Wine " + " in " + price
+
+	#print(WineType)
 
 
-		output_message = "Searched for " + color + " Wine"
-
-		#data = "You want " + query + " Wine " + " in " + price
-		if WineType is not None and Flavor_Additional is not None:
-			WineType = WineType + " " +Flavor_Additional
-		#print(WineType)
 
 		query = query_vec(WineType)
 		shortlist = shortlist_vin(query, color = color, price_min = int(MinPrice), price_max = int(MaxPrice))
-		data= shortlist['title'][:10].values.tolist()
+
+
+		data= shortlist[['title','price','sims','features']][:10]
+		lst = []
+		#for i in range(10):
+	#		lst.append(data.features[i])
+	#	list_of_path = WineCloud(lst)
+	#	data['path'] = list_of_path
+		data = data.values.tolist()
+		price = shortlist['price'][:10].values.tolist()
+		sim = shortlist['sims'][:10].values.tolist()
+
 
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data = data)
