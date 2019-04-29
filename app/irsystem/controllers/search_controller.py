bc5f0f13f01bf9a010a3b6dc54f5ac03f906f8ed
Farquhar13 <farquhar13@gmail.com>
2019-04-29 18:15:23 -0400

    6PM push

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -11,9 +11,7 @@ from sklearn.preprocessing import normalize
 from nltk import word_tokenize
 from nltk.util import ngrams
 import re
-#from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-#import matplotlib
-
+from numpy import linalg as la
 
 # relative path stuff
 import os
@@ -28,42 +26,55 @@ net_id = "...."
 
 @irsystem.route('/', methods=['GET'])
 def search():
+
 	#query = request.args.get('search')
-	WineType = request.args.get('flavor')
-	MinPrice = request.args.get('MinPrice')
+	WineType = request.args.getlist('flavor[]')
+	WineType = ' '.join(WineType)
 	MaxPrice = request.args.get('MaxPrice')
 	color  = request.args.get('color')
 	Flavor_Additional= request.args.get('Flavor_1')
+	Variety = request.args.get('variety')
+	rating = request.args.get('rating')
+	varietyfilter = request.args.get('varietyfilter')
+	Country = request.args.get('Country')
 	#print(MinPrice)
 	#print(WineType)
-	if not MinPrice:
-		MinPrice = 0
+	flag = []
+	flag3 =[]
 	if not MaxPrice:
 		MaxPrice = 3300
 	if not color:
 		color = None
 	#else:
-	print(MinPrice)
-	print(MaxPrice)
+	if not rating:
+		rating = 0
 
+	if not varietyfilter:
+		varietyfilter = 0
+	if not Country:
+		Country = None
 	price = 0
 	sim = 0
-	if not WineType and not Flavor_Additional:
+	if not WineType and not Flavor_Additional and not Variety:
+		flag3 = [1]
 		data = []
-		output_message = "Please enter a flavour"
+		output_message = "Please enter a flavor"
+
 	else:
 		if not WineType:
 			WineType = ""
 		if not Flavor_Additional:
 			Flavor_Additional = ""
 		WineType = WineType + " " +Flavor_Additional
-		print(WineType)
+		if not Variety:
+			Variety = ""
+		#print(WineType)
 
 	## comment to update code
 		master_data = pd.read_pickle(master_data_path)
-		vin_tfidf = np.load(vin_tfidf_path).item()
+		vin_tfidf = np.load(vin_tfidf_path ).item()
 		word_to_index = np.load(word_to_index_path).item()
-		#wine_mask = np.load('D:\\Cornell_Acads\\Second_Semester\\CS4300_Flask_template-master\\app\\irsystem\\controllers\\wine_mask.npy')
+		variety_tfidf = np.load(variety_tfidf_path).item()
 
 
 		def query_vec(s):
@@ -75,7 +86,7 @@ def search():
 			query_vec = np.zeros((1,len(word_to_index)))
 			for i,v in enumerate(query_token):
 				if v in word_to_index:
-					query_vec[0,word_to_index[v]] = 1
+					query_vec[0,word_to_index[v]] = 0.5
 
 			return query_vec
 
@@ -89,6 +100,7 @@ def search():
 
 			tfidf = vin_tfidf[:,subset]
 			query_vec = query_vec[0,subset]
+			query_vec = query_vec/la.norm(query_vec,ord=2)
 
 			sims = np.dot(tfidf.toarray(),query_vec.T)
 			sim_mat = pd.DataFrame(sims)
@@ -98,48 +110,81 @@ def search():
 			return sim_mat
 
 
-		def shortlist_vin(query_vec, color = None, price_min = None, price_max = None):
-			sim_mat = similar_vin(query_vec,vin_tfidf)
-			recco_mat = pd.merge(sim_mat, master_data, on='index', how='left')
+		def shortlist_vin(query_vec, color = None, price_min = None, price_max = None, rating = None,
+                  variety = None, variety_flag =0, country = None):
+					sim_mat = similar_vin(query_vec,vin_tfidf)
+					recco_mat = pd.merge(sim_mat, master_data, on='index', how='left')
 
-			if color != None:
-				recco_mat =  recco_mat[recco_mat.Color == color]
+					if color != None:
+						recco_mat =  recco_mat[recco_mat.Color == color]
 
-			if price_min != None and price_max != None:
-				recco_mat = recco_mat[recco_mat.price < price_max]
-				recco_mat = recco_mat[recco_mat.price > price_min]
+					if price_max != None:
+						recco_mat = recco_mat[recco_mat.price < price_max]
 
-			recco_mat.sort_values("sims", axis = 0, ascending = False,
-						 inplace = True)
+					if rating != None:
+						   recco_mat = recco_mat[recco_mat.points > rating]
 
-			return recco_mat[0:10]
+					if variety_flag == 1 and (variety in list(variety_tfidf.keys())):
+						 recco_mat = recco_mat[recco_mat['variety']==variety]
 
+					if country in ['Spain', 'US', 'New Zealand', 'France', 'Israel', 'Australia',
+					   'Portugal', 'Argentina', 'South Africa', 'Italy', 'Germany',
+					   'Greece', 'Moldova', 'Croatia', 'Serbia', 'Chile', 'Austria',
+					   'Georgia', 'Uruguay', 'Hungary', 'Ukraine', 'Brazil',
+					   'Slovenia', 'Canada', 'Morocco', 'Bulgaria', 'England',
+					   'Macedonia', 'Romania', 'Mexico', 'Turkey', 'China', 'Cyprus',
+					   'Slovakia', 'Lebanon', 'Switzerland', 'Luxembourg','Peru',
+					   'Egypt', 'Czech Republic', 'India', 'Armenia',
+					   'Bosnia and Herzegovina']:
+						recco_mat = recco_mat[recco_mat['country']==country]
+
+					recco_mat.sort_values("sims", axis = 0, ascending = False,
+								 inplace = True)
+
+					return recco_mat[0:10]
+
+	#	def WineCloud (df):
 
 
 		if color  ==None:
 			output_message = "Showing Results for "+ WineType + " Wine"
 		else:
 			output_message = "Showing Results for " + color + " " + WineType + " Wine"
+		if WineType == " " :
+			output_message = "Showing Results for " + Variety + " Wine"
+		elif color is not None and Variety is not None:
+			output_message = "Showing Results for " + color + " " + Variety + " "+ WineType + " Wine"
+		elif Variety is not None:
+			output_message = "Showing Results for "  + Variety + " "+ WineType + " Wine"
+
+		#print ("WineType is " + WineType)
+
 
 	#data = "You want " + query + " Wine " + " in " + price
 
 	#print(WineType)
+		#print(varietyfilter)
+		#print(Variety)
+
+		if Variety in list(variety_tfidf.keys()):
+			query = query_vec(WineType) + variety_tfidf[Variety].toarray()
+		else:
+			query = query_vec(WineType)
+
+		shortlist = shortlist_vin(query, color = color, price_max = int(MaxPrice), rating = int(rating),variety = Variety,variety_flag = int(varietyfilter), country = Country)
+
+		data= shortlist[['title','price','sims','description','points','country']][:10]
+		data['sims'] = data['sims'].apply(lambda x:round(x,3))
 
 
-
-		query = query_vec(WineType)
-		shortlist = shortlist_vin(query, color = color, price_min = int(MinPrice), price_max = int(MaxPrice))
-
-
-		data= shortlist[['title','price','sims','features']][:10]
-		lst = []
-		#for i in range(10):
-	#		lst.append(data.features[i])
-	#	list_of_path = WineCloud(lst)
+	#	list_of_path = WineCloud(data)
 	#	data['path'] = list_of_path
 		data = data.values.tolist()
 		price = shortlist['price'][:10].values.tolist()
 		sim = shortlist['sims'][:10].values.tolist()
 
+		if (len(data)== 0):
+			flag = [1]
 
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data = data)
+
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data = data, flag = flag,flag3 = flag3)
